f25fdd1efa2a5903e55ad71d723d3afd1f5bf4c2
bekzod818 <bekzod030900@gmail.com>
2022-08-08 06:56:46 +0500

    ready

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Group
 
 admin.site.register(User)
 admin.site.register(About)
 admin.site.register(Chance)
 admin.site.register(Coupon)
+admin.site.unregister(Group)
+# admin.site.unregister(User_1)
 
 @admin.register(Fakultet)
 class FakultetAdmin(admin.ModelAdmin):
